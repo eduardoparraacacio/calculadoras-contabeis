80783ccf2b6e1615c4a71a358ba1ffd7d66d46a0
eduardoparraacacio <duduparrinha07@gmail.com>
2026-05-20 11:24:29 -0300

    fizemos o desing

--- inss.js
+++ inss.js
@@ -1,12 +1,17 @@
-const salarioBruto = 3800
-let inss = 0
+const salarioBruto = 7500
+let inss = 0 
 
 if(salarioBruto <= 1621){
      inss = salarioBruto * 0.075
 }else if(salarioBruto > 1621 && salarioBruto <= 2902.84){
-     inss = (salarioBruto - 1621) * 0.09 + 121.58
+     inss = (salarioBruto - 1621)*0.09 + 121.58
 }else if(salarioBruto > 2902.84 && salarioBruto <= 4354.27){
-     inss = (salarioBruto - 2902.84) * 0.12 + 121.58 + 115.37
+     inss = (salarioBruto - 2902.84)*0.12 + 115.37 + 121.58
+}else if(salarioBruto > 4352.27 && salarioBruto <= 8475.55){
+     inss = (salarioBruto - 4354.27)*0.14 + 115.37 + 121.58 + 174.17
+}else{
+     inss = 988.10
 }
+
 
 console.log(inss.toFixed(2))
